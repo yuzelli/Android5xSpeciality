67099e4458d104890c4cd916b6e799e5f671e20e
yuzelli <zsl5155@qq.com>
2017-11-01 11:56:14 +0800

    ripple circular reveal

--- app/src/main/java/com/example/yuzelli/android5xspeciality/MainActivity.java
+++ app/src/main/java/com/example/yuzelli/android5xspeciality/MainActivity.java
@@ -17,10 +17,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.findViewById(R.id.btn_palette).setOnClickListener(this);
-
         this.findViewById(R.id.btn_etc).setOnClickListener(this);
         this.findViewById(R.id.btn_cardview).setOnClickListener(this);
         this.findViewById(R.id.btn_excessive).setOnClickListener(this);
+        this.findViewById(R.id.btn_material_design).setOnClickListener(this);
     }
 
     @Override
@@ -42,6 +42,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.btn_excessive:
                 intent.setClass(MainActivity.this, FristActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_material_design:
+                intent.setClass(MainActivity.this, MaterialDesignActivity.class);
                 startActivity(intent);
                 break;
             default:
---
+++ app/src/main/java/com/example/yuzelli/android5xspeciality/MaterialDesignActivity.java
@@ -0,0 +1,135 @@
+package com.example.yuzelli.android5xspeciality;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MaterialDesignActivity extends AppCompatActivity {
+    private Button btn_reval,btn_reval1;
+    private ImageView iv_reval,iv_reval1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_design);
+        btn_reval=(Button)findViewById(R.id.btn_reval);
+        btn_reval1=(Button)findViewById(R.id.btn_reval1);
+        iv_reval=(ImageView)findViewById(R.id.iv_reval);
+        iv_reval1=(ImageView)findViewById(R.id.iv_reval1);
+
+        btn_reval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(iv_reval.getVisibility() == View.GONE
+                        || iv_reval.getVisibility() == View.INVISIBLE){
+                    showRevalView();
+                }else{
+                    hideRevalView();
+                }
+            }
+        });
+        // 从某一角度到对角的动画
+        btn_reval1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(iv_reval1.getVisibility() == View.GONE||iv_reval1.getVisibility() == View.INVISIBLE){
+                    Animator anim =
+                            ViewAnimationUtils.createCircularReveal(
+                                    iv_reval1,//对应的view
+                                    0,// 开始缩放点x位置
+                                    0,// 开始缩放点y位置
+                                    0,//开始半径
+                                    // 结束半径    hypot(double ,double ) 表示斜线的长度
+                                    (float) Math.hypot(iv_reval1.getWidth(), iv_reval1.getHeight()));
+                    // make the view visible and start the animation
+                    iv_reval1.setVisibility(View.VISIBLE);
+                    anim.setDuration(2000);
+                    /* Set a natural ease-in/ease-out interpolator. */
+                    anim.setInterpolator(new AccelerateDecelerateInterpolator());
+                    anim.start();
+                }else{
+                    Animator anim =
+                            ViewAnimationUtils.createCircularReveal(
+                                    iv_reval1,//对应的view
+                                    0,// 开始缩放点x位置
+                                    0,// 开始缩放点y位置
+                                    // 开始半径和结束半径
+                                    (float) Math.hypot(iv_reval1.getWidth(), iv_reval1.getHeight()),0);
+                    // make the view visible and start the animation
+                    iv_reval1.setVisibility(View.VISIBLE);
+                    anim.setDuration(2000);
+                    /* Set a natural ease-in/ease-out interpolator. */
+                    anim.setInterpolator(new AccelerateDecelerateInterpolator());
+                    anim.start();
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            iv_reval1.setVisibility(View.GONE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+    protected void hideRevalView() {
+        // get the center for the clipping circle
+        int cx = (iv_reval.getLeft() + iv_reval.getRight()) / 2;
+        int cy = (iv_reval.getTop() + iv_reval.getBottom()) / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(iv_reval.getWidth(), iv_reval.getHeight());
+
+// create the animator for this view (the start radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(
+                        iv_reval,//对应的view
+                        iv_reval.getWidth()/2,// 开始缩放点x位置
+                        iv_reval.getHeight()/2,// 开始缩放点y位置
+                        iv_reval.getWidth(),// 开始半径和结束半径    hypot(double ,double ) 斜线的长度
+                        0);
+        // make the view visible and start the animation
+        iv_reval.setVisibility(View.VISIBLE);
+        anim.setDuration(2000);
+             /* Set a natural ease-in/ease-out interpolator. */
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+        anim.start();
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                iv_reval.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void showRevalView() {
+        // get the center for the clipping circle
+        int cx = (iv_reval.getLeft() + iv_reval.getRight()) / 2;
+        int cy = (iv_reval.getTop() + iv_reval.getBottom()) / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(iv_reval.getWidth(), iv_reval.getHeight());
+
+// create the animator for this view (the start radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(
+                        iv_reval,
+                        iv_reval.getWidth()/2,// 开始缩放点x位置
+                        iv_reval.getHeight()/2,// 开始缩放点y位置
+                        0,// 开始半径和结束半径
+                        (float) Math.hypot(iv_reval.getWidth(), iv_reval.getHeight()));
+        // make the view visible and start the animation
+        iv_reval.setVisibility(View.VISIBLE);
+        anim.setDuration(2000);
+         /* Set a natural ease-in/ease-out interpolator. */
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+        anim.start();
+    }
+
+}
